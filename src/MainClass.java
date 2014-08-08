a69bdf9e3e17c2a62857db229c8b4d3b5cadea4e
vinicioflores <vfloreshdz@gmail.com>
2014-08-07 21:17:11 -0600

    classpath

---
+++ src/MainClass.java
@@ -0,0 +1,13 @@
+
+
+/** 
+ * En este archivo se encuentra la clase principal del programa de gestión bibliotecario Alejandría. 
+ *  Desde este módulo se corre todo el programa  
+ *  
+ *  **/
+
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println("Cargando sistema bibliotecario Alejandria ...");
+	}	
+}
