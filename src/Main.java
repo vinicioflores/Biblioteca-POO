036e70107e26aaaaa14840d3faa7e0f27c4823c3
vinicioflores <vfloreshdz@gmail.com>
2014-08-11 16:28:24 -0600

    Unificacion
    Se une la interfaz con la base del programa en Java

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,20 @@
  *  
  *  **/
 
+/** .l. */
+
 public class Main {
 	private static Registro padron;
 	private static Libreria coleccion;
 	private static int MAX_CLIENTS = 9000;
 	private static int MAX_BOOKS   = 18000;
+	private static Interfaz MainWin = null; 
 
 	public static void main(String[] args){
 		
 		System.out.println("Cargando sistema bibliotecario Alejandria ...");
 		padron = new Registro(MAX_CLIENTS);
 		coleccion = new Libreria(MAX_BOOKS);
-		coleccion.getBooksInfo();
-		
-		Libro book0 = new Libro();
-		
-		book0.setTitulo("math1");
-		book0.setAutores("Murillo Ts");
-		book0.setEditorial("ETCR");
-		book0.setEdicion(4);
-		book0.setScore(9);
-		
-		coleccion.write_book_register("collection.regf", book0, true);
+		MainWin = new Interfaz();
 	}	
 }
