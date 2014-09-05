83577cb446cb9af2c8bcfa002cb5bac11384553b
WolfangAukang <de.oliveira.rodriguez@gmail.com>
2014-09-05 08:55:42 -0600

    Correcciones en ortografía de código

--- src/app/model/Pelicula.java
+++ src/app/model/Pelicula.java
@@ -4,15 +4,15 @@ public class Pelicula extends Pertenencia{
 	
 	String genero;
 	String director;
-	String añoPublicacion;
+	String anhoPublicacion;
 	
 	public Pelicula(String nombre, String imagen, int calificacion,
 			int vecesPrestado, Prestatario prestatario, String genero,
-			String director, String añoPublicacion) {
+			String director, String anhoPublicacion) {
 		super(nombre, imagen, calificacion, vecesPrestado, prestatario);
 		this.genero = genero;
 		this.director = director;
-		this.añoPublicacion = añoPublicacion;
+		this.anhoPublicacion = anhoPublicacion;
 	}
 
 	public String getGenero() {
@@ -31,27 +31,20 @@ public class Pelicula extends Pertenencia{
 		this.director = director;
 	}
 
-	public String getAñoPublicacion() {
-		return añoPublicacion;
+	public String getanhoPublicacion() {
+		return anhoPublicacion;
 	}
 
-	public void setAñoPublicacion(String añoPublicacion) {
-		this.añoPublicacion = añoPublicacion;
+	public void setanhoPublicacion(String anhoPublicacion) {
+		this.anhoPublicacion = anhoPublicacion;
 	}
 
 	@Override
 	public String toString() {
 		return "Pelicula [genero=" + genero + ", director=" + director
-				+ ", añoPublicacion=" + añoPublicacion + ", nombre=" + nombre
+				+ ", anhoPublicacion=" + anhoPublicacion + ", nombre=" + nombre
 				+ ", imagen=" + imagen + ", calificacion=" + calificacion
 				+ ", vecesPrestado=" + vecesPrestado + ", prestatario="
 				+ prestatario + "]";
 	}
-	
-	
-	
-	
-	
-	
-
 }
