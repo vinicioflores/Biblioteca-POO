bf9be0a7307c22f108b78b615d4df9ad268eb9f4
Faubricio Forester <fkahuman@gmail.com>
2014-09-08 20:09:55 -0600

    Creación de clase "Reporte"

---
+++ src/app/view/Reporte.java
@@ -0,0 +1,30 @@
+package app.view;
+
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JTable;
+
+public class Reporte extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Reporte() {
+		setLayout(new MigLayout("", "[grow]", "[][grow][][][][][][][][]"));
+		
+		JLabel lblReporte = new JLabel("Reporte de libros prestados");
+		add(lblReporte, "cell 0 0");
+		
+		JTextArea textArea = new JTextArea();
+		add(textArea, "cell 0 1 1 8,grow");
+		
+		JButton btnAceptar = new JButton("Cerrar");
+		add(btnAceptar, "flowx,cell 0 9");
+
+	}
+
+}
