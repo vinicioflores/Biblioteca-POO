f34be1d80642450a189bc97c7770f141e71f28b7
Faubricio Forester <fkahuman@gmail.com>
2014-09-09 21:21:55 -0600

    Borrado del main de la clase "View"

--- src/app/view/View.java
+++ src/app/view/View.java
@@ -30,18 +30,7 @@ public class View extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					View frame = new View();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the frame.
