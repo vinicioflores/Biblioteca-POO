0be18e1aa062acf273d18a51f7d3a94370d00c6c
Pedro Rodríguez de Oliveira (WolfangAukang) <de.oliveira.rodriguez@gmail.com>
2014-09-11 21:07:03 -0600

    Se ha agregado el ícono del programa a la ventana.

--- src/app/view/View.java
+++ src/app/view/View.java
@@ -60,7 +60,7 @@ public class View extends JFrame {
 	 * Create the frame.
 	 */
 	public View() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(View.class.getResource("/image/logo_small.png")));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(View.class.getResource("/image/icon.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 747, 524);
 		
