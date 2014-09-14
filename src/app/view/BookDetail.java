a8ed0c95b7a1ddc45894d4c4791fb794daa538c0
Faubricio Forester <fkahuman@gmail.com>
2014-09-14 10:22:00 -0600

    Ejemplo de panel de búsqueda

--- src/app/view/BookDetail.java
+++ src/app/view/BookDetail.java
@@ -1,14 +1,18 @@
 package app.view;
 
 import javax.swing.JPanel;
+
+import app.model.Libro;
 import net.miginfocom.swing.MigLayout;
 
 public class BookDetail extends JPanel {
-
+	
+	private Libro book;
 	/**
 	 * Create the panel.
 	 */
-	public BookDetail() {
+	public BookDetail(Libro book) {
+		this.book = book;
 		setLayout(new MigLayout("", "[]", "[]"));
 
 	}
